55a4ceca8dea3bba28c12f5ae7b00ea021365c0a
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-11 16:56:53 -0500

    add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "cvillad", password: "password")
+User.create(username: "krlos9925", password: "password")
+User.create(username: "RnJohn", password: "password")

7a5db1959befde4c4d2c46b8909b77865fba2991
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-12 09:19:30 -0500

    add flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,7 @@
 
 $(document).on("turbolinks:load", ()=>{
   $(".ui.dropdown").dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 });
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
   def new 
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id]=user.id
+      flash[:success]="Logged in successfully"
+      redirect_to root_path
+    else
+      flash.now[:error]="Username or password incorrect"
+      render :new
+    end
+    
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out successfully"
+    redirect_to login_path
+  end
+
 end
